7913fa64c830729859ce41c1f81caa014f5ef73c
Sérgio Rebelo <sergiorebelo@me.com>
2024-11-04 14:16:05 +0000

    update index.js (action) #8

--- .github/actions/hello-world/index.js
+++ .github/actions/hello-world/index.js
@@ -1,8 +1,11 @@
 const core = require('@actions/core');
 
 try {
-    const name = core.getInput('name') || 'World';
-    console.log(`Hello, ${name}!`);
+    const name = core.getInput('who-to-greet');
+    console.log(`Hello ${name}!`);
+    core.setOutput('time', new Date().toTimeString());
 } catch (error) {
     core.setFailed(error.message);
 }
+
+
